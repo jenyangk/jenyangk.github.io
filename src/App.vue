1d34e4ecf859922c474e301d8b3a891247920b3f
Jen Yang Koh <jenyang@ualberta.ca>
2022-04-21 22:43:50 -0600

    Revamp init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap");
+
+#app {
+  font-family: IBM Plex Mono, monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+body {
+  color: white;
+  background: rgb(34, 36, 41);
+  font-weight: 400;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: white;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
