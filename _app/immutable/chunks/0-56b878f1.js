d53f4ca2015d95f8022520823f13715ef11d9fcc
Jen Yang Koh <jenyang@ualberta.ca>
2023-02-11 15:24:05 -0700

    Updates

--- _app/immutable/chunks/0-56b878f1.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_layout-da46b06b.js";import{default as t}from"../components/pages/_layout.svelte-ca3bc7b5.js";export{t as component,r as universal};
--- _app/immutable/chunks/2-6ff11798.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-3457e2b9.js";export{t as component};
